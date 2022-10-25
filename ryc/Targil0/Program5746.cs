69a783e8faeb0b1fcdd25004274e598892ef97ca
rvkk2828 <rvkk2828@gmail.com>
2022-10-25 20:59:01 +0300

    initial code + Welcome functions

---
+++ ryc/Targil0/Program5746.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Targil0
+    {
+        static void Main(string[] args)
+        {
+            Welcome5746();
+            Welcome3923();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3923();
+
+        private static void Welcome5746()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", name);
+        }
+    }
+}
